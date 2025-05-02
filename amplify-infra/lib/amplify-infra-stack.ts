a10d8cffccd3c765bb6a2a71f1ff79bb232dbe0d
George Wilson <george.wilson@version1.com>
2025-05-02 23:54:05 +0100

    Working code build setup

--- amplify-infra/lib/amplify-infra-stack.ts
+++ amplify-infra/lib/amplify-infra-stack.ts
@@ -1,21 +1,27 @@
-import * as cdk from "aws-cdk-lib";
-import * as codecommit from "aws-cdk-lib/aws-codecommit";
+import * as cdk from 'aws-cdk-lib';
+import * as codecommit from 'aws-cdk-lib/aws-codecommit';
 import { Construct } from 'constructs';
+import * as amplify from '@aws-cdk/aws-amplify-alpha';
 
 export class AmplifyInfraStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
-    // Part 1 [Optional] - Creation of the source control repository
-    const amplifyReactSampleRepo = new codecommit.Repository(
-        this,
-        "AmplifyReactTestRepo",
-        {
-          repositoryName: "georgewilson-amplify-react-todo-app",
-          description:
-              "CodeCommit repository that will be used as the source repository for the todo react app for the AWS Team May 2025 challenge",
-        }
-    );
-  }
+        const todoRepo = new codecommit.Repository(
+            this,
+            'AmplifyTodoTestApp',
+            {
+                repositoryName: 'georgewilson-amplify-react-todo-app',
+                description:
+                    'CodeCommit repository that will be used as the source repository for the todo react app for the AWS Team May 2025 challenge',
+            }
+        );
+
+        const amplifyApp = new amplify.App(this, 'todo-react-app ', {
+            sourceCodeProvider: new amplify.CodeCommitSourceCodeProvider({
+                repository: todoRepo,
+            }),
+        });
+        amplifyApp.addBranch('main');
+    }
 }
-
