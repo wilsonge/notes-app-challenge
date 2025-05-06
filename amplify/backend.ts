9b2df9ad95afea149270d997203d811de86f2b1a
George Wilson <george.wilson@version1.com>
2025-05-07 00:00:37 +0100

    Simplify config

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -11,26 +11,8 @@ const backend = defineBackend({
 backend.auth.resources.unauthenticatedUserIamRole.addToPrincipalPolicy(
     new PolicyStatement({
         actions: [
-            "translate:TranslateText",
             "polly:SynthesizeSpeech",
             "transcribe:StartStreamTranscriptionWebSocket",
-            "comprehend:DetectSentiment",
-            "comprehend:DetectEntities",
-            "comprehend:DetectDominantLanguage",
-            "comprehend:DetectSyntax",
-            "comprehend:DetectKeyPhrases",
-            "rekognition:DetectFaces",
-            "rekognition:RecognizeCelebrities",
-            "rekognition:DetectLabels",
-            "rekognition:DetectModerationLabels",
-            "rekognition:DetectText",
-            "rekognition:DetectLabel",
-            "rekognition:SearchFacesByImage",
-            "textract:AnalyzeDocument",
-            "textract:DetectDocumentText",
-            "textract:GetDocumentAnalysis",
-            "textract:StartDocumentAnalysis",
-            "textract:StartDocumentTextDetection",
         ],
         resources: ["*"],
     })
@@ -40,58 +22,9 @@ backend.addOutput({
     custom: {
         Predictions: {
             convert: {
-                translateText: {
-                    defaults: {
-                        sourceLanguage: "en",
-                        targetLanguage: "es",
-                    },
-                    proxy: false,
-                    region: backend.auth.stack.region,
-                },
-                speechGenerator: {
-                    defaults: {
-                        voiceId: "Ivy",
-                    },
-                    proxy: false,
-                    region: backend.auth.stack.region,
-                },
                 transcription: {
                     defaults: {
-                        language: "en-US",
-                    },
-                    proxy: false,
-                    region: backend.auth.stack.region,
-                },
-            },
-            identify: {
-                identifyEntities: {
-                    defaults: {
-                        collectionId: "default",
-                        maxEntities: 10,
-                    },
-                    celebrityDetectionEnabled: true,
-                    proxy: false,
-                    region: backend.auth.stack.region,
-                },
-                identifyLabels: {
-                    defaults: {
-                        type: "ALL",
-                    },
-                    proxy: false,
-                    region: backend.auth.stack.region,
-                },
-                identifyText: {
-                    defaults: {
-                        format: "ALL",
-                    },
-                    proxy: false,
-                    region: backend.auth.stack.region,
-                },
-            },
-            interpret: {
-                interpretText: {
-                    defaults: {
-                        type: "ALL",
+                        language: "en-GB",
                     },
                     proxy: false,
                     region: backend.auth.stack.region,
