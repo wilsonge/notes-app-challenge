bf139d3678265c494cf922e64297398b6e88e3bc
George Wilson <george.wilson@version1.com>
2025-05-06 18:52:55 +0100

    Remove lots of the styling

--- src/components/Screens.tsx
+++ src/components/Screens.tsx
@@ -33,50 +33,6 @@ const SignOutButton = styled(Button)`
   cursor: pointer;
 `;
 
-const StyledTabList = styled(TabList)`
-  display: flex;
-  justify-content: stretch;
-  align-items: center;
-  position: fixed;
-  top: 80px;
-  left: 0;
-  right: 0;
-  height: 40px;
-  box-shadow: 0 6px 6px rgba(116, 180, 155, 0.4);
-
-  & > [data-selected] {
-    border-bottom-color: #74b49b;
-    color: #74b49b;
-  }
-`;
-
-const StyledTabPanels = styled.div`
-  padding-top: 50px;
-  flex: 1;
-
-  [hidden] {
-    display: none;
-  }
-`;
-
-const StyledTabPanel = styled(TabPanel)`
-  flex: 1;
-  padding: 16px;
-  display: flex;
-  min-height: calc(100% - 32px);
-`;
-
-const StyledTab = styled(Tab)`
-  text-transform: uppercase;
-  flex: 1;
-  padding: 16px;
-  color: #74b49b;
-  background-color: #ffffff;
-  font-size: 16px;
-  border: none;
-  border-bottom: 3px solid #ffffff;
-`;
-
 const Screens = () => {
     const tab = useTabStore();
     const defaultSelectedId = "default-selected-tab";
@@ -95,18 +51,18 @@ const Screens = () => {
                 </SignOutButton>
             </Header>
             <TabProvider defaultSelectedId={defaultSelectedId}>
-                <StyledTabList store={tab}>
-                    <StyledTab id={defaultSelectedId}>Notes</StyledTab>
-                    <StyledTab>Record</StyledTab>
-                </StyledTabList>
-                <StyledTabPanels>
-                    <StyledTabPanel tabId={defaultSelectedId}>
+                <TabList store={tab}>
+                    <Tab id={defaultSelectedId}>Notes</Tab>
+                    <Tab>Record</Tab>
+                </TabList>
+                <div className="panels">
+                    <TabPanel tabId={defaultSelectedId}>
                         <Notes />
-                    </StyledTabPanel>
-                    <StyledTabPanel>
+                    </TabPanel>
+                    <TabPanel>
                         <Record />
-                    </StyledTabPanel>
-                </StyledTabPanels>
+                    </TabPanel>
+                </div>
             </TabProvider>
         </>
     );
