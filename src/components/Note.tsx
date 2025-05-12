0bc17530f74f7fe32602ab663b9df2dbee57d46e
George Wilson <george.wilson@version1.com>
2025-05-12 01:09:55 +0100

    Fix final icons

--- src/components/Note.tsx
+++ src/components/Note.tsx
@@ -116,13 +116,13 @@ const NotesComponent = (props: NoteComponentProps) => {
             <Divider />
             <NoteActions>
                 <ButtonIcon onClick={() => playAudio()}>
-                    <Icon viewBox={{width: 448, height: 512 }} as={FaPlay} />;
+                    <Icon viewBox={{width: 448, height: 512 }} as={FaPlay} />
                 </ButtonIcon>
                 <ButtonIcon onClick={() => setShowEditor(true)}>
-                    <Icon viewBox={{width: 448, height: 512 }} as={FaRegEdit} />;
+                    <Icon viewBox={{width: 576, height: 512 }} as={FaRegEdit} />
                 </ButtonIcon>
                 <ButtonIcon>
-                    <Icon viewBox={{width: 448, height: 512 }} as={FaRegTrashAlt} onClick={props.onDelete} />;
+                    <Icon viewBox={{width: 448, height: 512 }} as={FaRegTrashAlt} onClick={props.onDelete} />
                 </ButtonIcon>
             </NoteActions>
 
