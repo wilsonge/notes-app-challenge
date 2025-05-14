2ea9c94d0297681b24bb62452a8f18a2cde4fdd2
George Wilson <george.wilson@version1.com>
2025-05-14 09:30:36 +0100

    Integrate tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
+        tailwindcss(),
         nodePolyfills({
             globals: {
                 Buffer: true,
