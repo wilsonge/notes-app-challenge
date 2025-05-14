292419ba7e9e72ea211bf9541e58712fa4284aad
George Wilson <georgejameswilson@googlemail.com>
2025-05-14 12:53:03 +0100

    Tailwind after react

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
-        tailwindcss(),
         nodePolyfills({
             globals: {
                 Buffer: true,
@@ -14,6 +13,7 @@ export default defineConfig({
                 process: true,
             },
         }),
-        react()
+        react(),
+        tailwindcss(),
     ],
 })
