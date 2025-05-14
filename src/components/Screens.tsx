265261b9819affbcf5de1c822bdd930574aef008
George Wilson <george.wilson@version1.com>
2025-05-14 14:11:33 +0100

    Fully tailwind screen component

--- src/components/Screens.tsx
+++ src/components/Screens.tsx
@@ -1,16 +1,9 @@
-import styled from "@emotion/styled";
 import { Button } from '@aws-amplify/ui-react';
 import { signOut } from 'aws-amplify/auth';
 import { TabProvider, TabList, Tab, TabPanel, useTabStore } from "@ariakit/react";
 
 import Notes from "./Notes";
 import Record from "./Record";
-
-const Header = styled("div")`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-`;
 
 const Screens = () => {
     const tab = useTabStore();
@@ -22,13 +15,13 @@ const Screens = () => {
 
     return (
         <>
-            <Header>
+            <div className="d-flex align-items-center justify-between">
                 <h1 className="text-v1-teal uppercase mb-0 mt-0">Quick Notes</h1>
                 <Button className="background-v1-teal"
                     onClick={handleSignOut}>
                     Sign Out
                 </Button>
-            </Header>
+            </div>
             <TabProvider defaultSelectedId={defaultSelectedId}>
                 <TabList className="tab-list" store={tab}>
                     <Tab className="tab" id={defaultSelectedId}>Notes</Tab>
